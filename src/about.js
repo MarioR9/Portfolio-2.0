c8cf5c4b45648680df240c46f0956cd50b40df61
MarioR9 <mariorodriguezan@gmail.com>
2019-11-27 20:13:09 -0500

    removing social links from the sides

--- src/about.js
+++ src/about.js
@@ -17,13 +17,6 @@ export default class About extends React.Component{
                   <p class="aboutcontainer" >Full-stack web developer with a passion for problem-solving. With experience in Ruby on Rails, JavaScript, and React I discovered web development while taking computer science classes and I fell in love with it. I bring strong skills in working with people, communication in English and Spanish as well as managing people. I spend my free time playing guitar as well as building small projects and learning about new technologies.</p>
                 </Container>
               </div>
-             
-              <div class="sidelinks">
-                <a href="https://www.linkedin.com/in/mario-rodriguezan/">Linkedin</a>
-              </div>
-              <div class="sidelinks2">
-                <a href="https://github.com/MarioR9">Github</a>
-              </div>
             </div>
         )
     }
